96416cb68c93f044562559164f5abb07e93a90f7
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-15 23:22:43 -0300

    Add

--- Exercices/Dicee Challenge/index.js
+++ Exercices/Dicee Challenge/index.js
@@ -1,9 +1,16 @@
-randomNumber1 = Math.floor(Math.random() * 6)+1;
+let randomNumber1 = Math.floor(Math.random()*6)+1
+let randomNumber2 = Math.floor(Math.random()*6)+1
 
-var randomDiceImage = "dice" + randomNumber1 + ".png";
+let randomDice1 = "images/dice"+randomNumber1+".png"
+let randomDice2 = "images/dice"+randomNumber2+".png"
 
-var randomImageSource = "images/" + randomDiceImage;
+document.getElementsByClassName("img1")[0].setAttribute("src",randomDice1)
+document.getElementsByClassName("img2")[0].setAttribute("src",randomDice2)
 
-var image1 = document.querySelectorAll("img")[0];
-
-image1.setAttribute("src", randomImageSource);
+if(randomNumber1>randomNumber2){
+    document.querySelector("h1").innerHTML = "player 1 wins"
+}else if(randomNumber2 > randomNumber1){
+    document.querySelector("h1").innerHTML = "player 2 wins"
+}else{
+    document.querySelector("h1").innerHTML = "DRAW"
+}
