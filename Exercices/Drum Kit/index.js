8bc1b3b26b1f24099b09c67b598e22126dd6ba87
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-18 23:48:56 -0300

    Add

--- Exercices/Drum Kit/index.js
+++ Exercices/Drum Kit/index.js
@@ -3,7 +3,7 @@ let numberOfDrumBtn = document.querySelectorAll(".drum").length
 for (let i = 0; i < numberOfDrumBtn; i++){
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
-        alert("clicado")
+        alert("clicado ok")
     })
 
 }
