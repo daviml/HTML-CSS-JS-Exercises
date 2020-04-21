20d68d670a6f80b0f4b57de2fe0dfde954676bd1
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-21 13:18:25 -0300

    Add

--- Exercices/Drum Kit/index.js
+++ Exercices/Drum Kit/index.js
@@ -9,6 +9,7 @@ for (let i = 0; i < numberOfDrumBtn; i++){
         let buttonInnerHTML = this.innerHTML    
 
         makeSound(buttonInnerHTML)
+        buttonAnimation(buttonInnerHTML)
 
     })
 
@@ -17,6 +18,7 @@ for (let i = 0; i < numberOfDrumBtn; i++){
 //detect keyboard press
 document.addEventListener("keypress", function(event){
     makeSound(event.key)
+    buttonAnimation(event.key)
 })
 
 
@@ -57,3 +59,16 @@ function makeSound(key){
             break;
     }
 }
+
+function buttonAnimation(currentKey){
+
+var activeButton = document.querySelector("." + currentKey);
+
+activeButton.classList.add("pressed");
+
+setTimeout(function(){
+    activeButton.classList.remove("pressed")
+}, 100)
+
+}
+
