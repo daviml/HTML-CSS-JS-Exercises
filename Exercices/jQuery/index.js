7765aad596c9f78b7193328dfd388ebbb800dad9
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-23 21:11:37 -0300

    Add

--- Exercices/jQuery/index.js
+++ Exercices/jQuery/index.js
@@ -1,1 +1,20 @@
-$("h1").addClass("big-t")
+$("h1").click(function(){
+    $("h1").css("color", "red")
+})
+
+$("button").click(function(){
+    $("h1").css("color","purple")
+})
+
+$(document).keypress(function(event){
+    
+    $("h1").text(event.key)
+})
+
+$("h1").on("mouseover", function(){
+    $("h1").css("color","black")
+})
+
+$("h1").before("<button>NEW</button>")
+
+$("h1").after("<button>NEW</button>")
