48960e223dd7f5d6a60c4c77cc0d19679981a518
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-23 23:29:15 -0300

    Add

---
+++ Exercices/Simon Game Challenge/game.js
@@ -0,0 +1,18 @@
+let buttonColours = ["red","blue","green","yellow"]
+
+let gamePattern = []
+
+
+
+    let randomNumber = Math.floor(Math.random() * 4)
+
+    let randomChosenColour = buttonColours[randomNumber]
+
+    gamePattern.push(randomChosenColour);
+
+    $("#"+randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100)
+
+    var audio = new Audio("sounds/blue.mp3");
+  audio.play();
+
+
