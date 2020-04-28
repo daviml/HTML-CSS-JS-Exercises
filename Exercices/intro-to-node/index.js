517f4070a48d333df6b20e4cfd5b173300c62576
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-28 00:30:51 -0300

    Add

--- Exercices/intro-to-node/index.js
+++ Exercices/intro-to-node/index.js
@@ -3,3 +3,9 @@ var superheroes = require("superheroes")
 var mySup = superheroes.random()
 
 console.log(mySup)
+
+console.log("vs")
+
+var marv = require("marvel-characters")
+
+console.log(marv())
