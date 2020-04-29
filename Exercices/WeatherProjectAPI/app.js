d09d08095aa0972516796a5e8d505ffbf2785b34
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-29 10:59:37 -0300

    Add

--- Exercices/WeatherProjectAPI/app.js
+++ Exercices/WeatherProjectAPI/app.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const https = require('https')
+const bodyParser = require("body-parser")
 
 const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get('/', function(req,res){
 
@@ -10,29 +13,31 @@ app.get('/', function(req,res){
 })
 
 app.post("/",function(req,res){
-    console.log("post recived")
+
+    const city = req.body.CityName
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&appid=3c7d60a6066e2361844a5442e0cab21f"
+
+    https.get(url, function(response){
+    console.log(response.statusCode)
+
+    response.on('data',function(data){
+        const weatherData = JSON.parse(data)
+        const temp = weatherData.main.temp
+        const weatherDescription = weatherData.weather[0].description
+        const icon = weatherData.weather[0].icon
+        const imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
+        
+        res.write("<p>Temp: "+temp+"<p>")
+        res.write('<p>Weather is: '+weatherDescription+"<p>")
+        res.write("<img src="+imgURL+">")
+
+        res.send()
+    })
+})
+    
 })
 
-// const city = "Uberlandia"
-// const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city + "&units=metric&appid=3c7d60a6066e2361844a5442e0cab21f"
 
-// https.get(url, function(response){
-//     console.log(response.statusCode)
-
-//     response.on('data',function(data){
-//         const weatherData = JSON.parse(data)
-//         const temp = weatherData.main.temp
-//         const weatherDescription = weatherData.weather[0].description
-//         const icon = weatherData.weather[0].icon
-//         const imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png"
-        
-//         res.write("<p>Temp: "+temp+"<p>")
-//         res.write('<p>Weather is: '+weatherDescription+"<p>")
-//         res.write("<img src="+imgURL+">")
-
-//         res.send()
-//     })
-// })
 
 
 
