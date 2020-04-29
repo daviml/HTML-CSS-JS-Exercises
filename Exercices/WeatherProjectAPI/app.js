9bbbfe5353e76d353a3d12f5e7bba72fd44b0284
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-04-29 09:46:31 -0300

    Add

--- Exercices/WeatherProjectAPI/app.js
+++ Exercices/WeatherProjectAPI/app.js
@@ -1,8 +1,22 @@
 const express = require('express')
+const https = require('https')
 
 const app = express()
 
 app.get('/', function(req,res){
+
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Uberlandia&units=metric&appid=3c7d60a6066e2361844a5442e0cab21f'
+    
+    https.get(url, function(response){
+        console.log(response.statusCode)
+
+    response.on('data',function(data){
+        const weatherData = JSON.parse(data)
+        console.log(weatherData)
+    })
+
+    })
+
     res.send('server is up and runing.')
 })
 
