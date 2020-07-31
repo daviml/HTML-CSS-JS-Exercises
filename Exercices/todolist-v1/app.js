421390e37651e19ab266ec194f4ad1ff10d63b3d
Davi Moraes Ladeira <davimoraesladeira@hotmail.com>
2020-07-31 17:38:19 -0300

    Add

--- Exercices/todolist-v1/app.js
+++ Exercices/todolist-v1/app.js
@@ -1,10 +1,38 @@
-var express = require("express");
-var bodyParser = require("body-parser");
+let express = require("express");
+let bodyParser = require("body-parser");
 
-var app = express();
+let app = express();
+
+let items = ["compra","cozinha","come"];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
-    res.send("Hello World");
+    
+let today = new Date();
+let currentday = today.getDay();
+
+let options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long"
+};
+
+var day = today.toLocaleDateString("pt-BR", options);
+
+res.render("list",{KindOfDay: day, newListItems: items });
+
+});
+
+app.post("/", function(req,res){
+    let item = req.body.newItem;
+    
+    items.push(item);
+    
+    res.redirect("/");
 });
 
 app.listen(3000, function(){
